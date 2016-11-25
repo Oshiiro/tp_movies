b528d5627c50026a5771711ae2269365f71c1f9d
tatain1 <tatain1@hotmail.com>
2016-11-25 15:54:49 +0100

    Revision de single

--- single.php
+++ single.php
@@ -6,27 +6,31 @@
 
 // FICHE TECHNIQUE DU FILM
 
-  if(!empty($_GET['id']) && is_numeric($_GET['id'])) {
-    $id = $_GET['id'];
+  if(!empty($_GET['slug'])) {
+    $slug = $_GET['slug'];
 
-    $sql = "SELECT * FROM movies_full WHERE id=:id";
+    $sql = "SELECT * FROM movies_full WHERE slug=:slug";
     $query = $pdo->prepare($sql);
-    $query->bindValue(':id', $id, PDO::PARAM_INT);
+    $query->bindValue(':slug', $slug, PDO::PARAM_INT);
     $query->execute();
     $movies = $query->fetchAll();
 
 
     foreach ($movies as $movie) {
-      if($_GET['id'] == $movie['id']) { ?>
+      if($_GET['slug'] == $movie['slug']) { ?>
 
         <div class="col-lg-6 col-lg-offset-3">
-          <h4>Nom du film</h4>
+          <h4><?php echo $movie['title'] ?></h4>
           <img src="posters/<?php echo $movie['id'] ?>.jpg" alt="" class="imagesingle">
           <div class="infofilm col-lg-9">
             <h5> Infos du film : </h5>
             <hr>
-            <p>Année : <?php echo $movie['year']?></p>
             <p>Résumé : <?php echo $movie['plot']?></p>
+            <p>Réalisateur : <?php echo $movie['directors']?></p>
+            <p>Acteurs principaux : <?php echo $movie['cast']?></p>
+            <p>Durée : <?php echo $movie['runtime']. ' minutes'?></p>
+            <p>Année : <?php echo $movie['year']?></p>
+            <p>Genre : <?php echo $movie['genres']?></p>
             <p>Note : <?php echo $movie['rating']. '/100'?></p>
           </div>
         </div>
