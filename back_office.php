9d0987d80d23acae3c0c0d2c37c5397ee830a040
Big-Bud <webforce321@gmail.com>
2016-11-25 11:33:54 +0100

    test01

--- back_office.php
+++ back_office.php
@@ -1,1 +1,13 @@
+<?php require('include/pdo.php') ?>
 <?php require('include/header_back.php') ?>
+<?php
+
+
+ ?>
+ <nav class="container">
+   <div class="">
+     <div class="alert alert-success" role="alert">Nombres d'utilisateurs : <?php  ?></div>
+     <div class="alert alert-success" role="alert">Dernier inscrit : <?php  ?></div>
+     <div class="alert alert-success" role="alert">Nombres de film: <?php  ?></div>
+   </div>
+ </nav>
