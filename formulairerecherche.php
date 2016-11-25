b7b9695b0eb698d5f977aede7ab2bfb55dcc0f80
J-Hell-P <jerryleeperree@gmail.com>
2016-11-25 15:20:49 +0100

    blabla

--- formulairerecherche.php
+++ formulairerecherche.php
@@ -1,28 +1,3 @@
-<?php include ('include/pdo.php'); ?>
-<?php include ('include/functions.php'); ?>
-<?php
-
-// traitement du randow
-$sql = "SELECT id FROM `movies_full`
-        ORDER BY RAND()
-        LIMIT 5";
-  $query = $pdo->prepare($sql);
-  $query->execute();
-  $randomId = $query->fetchAll();
-
-
-// traitement du bouton + de film
-if (!empty($_POST['plusDeFilm'])) {
-  $sql = "SELECT * FROM `movies_full`
-          ORDER BY RAND()
-          LIMIT 5";
-    $query = $pdo->prepare($sql);
-    $query->execute();
-    $randomId2 = $query->fetchAll();
-}
- ?>
-<?php include ('include/header.php'); ?>
-
 <div class="choixchoix col-lg-6 col-lg-offset-3">
 
   <div class ="categories col-lg-4" >
@@ -96,30 +71,3 @@ if (!empty($_POST['plusDeFilm'])) {
       <i class="fa fa-star" aria-hidden="true"></i>
     </label>
   </div>
-
-
-  <div class="col-lg-12 films" style="text-align : center">
-    <h4>Téma ça mon srab</h4>
-    <a href="single.php">  <?php if (!empty($_POST['plusDeFilm'])) { ?>
-      <?php foreach ($randomId2 as $key): ?>
-        <img src="posters/<?php echo $key['id'] ?>.jpg" alt="">
-      <?php endforeach; ?>
-      <?php } else { ?>
-        <?php foreach ($randomId as $key): ?>
-          <img src="posters/<?php echo $key['id'] ?>.jpg" alt="">
-        <?php endforeach; ?>
-        <?php } ?></a>
-      </div>
-
-      <div class="choix col-lg-12">
-        <form action="" method="post">
-          <div class="boutonrandom col-lg-12" style="text-align : center">
-            <input type="submit" name="plusDeFilm" class="btn btn-success" value="+ De FILM">
-          </div>
-        </form>
-      </div>
-
-</div> <!-- Div CHOIXCHOIX -->
-<!-- Notes -->
-
-<?php include 'include/footer.php'; ?>
