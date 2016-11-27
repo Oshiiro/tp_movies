18c3c08a12f32b11496fcbdfe29224b5006eadcb
Kilian Tatain Mirail <tatain1@hotmail.com>
2016-11-27 16:24:13 +0100

    modif du WE by Kiki

---
+++ avoir.php
@@ -0,0 +1,28 @@
+<?php include 'include/pdo.php'; ?>
+<?php include 'include/functions.php'; ?>
+
+<?php
+$idSession = $_SESSION['user']['id'];
+
+// traitement du randow
+$sql = "SELECT * FROM movies_user_note WHERE status = 1 AND id_user = :id_user";
+  $query = $pdo->prepare($sql);
+  $query->bindValue(':id_user', $idSession, PDO::PARAM_INT);
+  $query->execute();
+  $aVoirs = $query->fetchAll();
+
+?>
+
+<?php require('include/header.php') ?>
+
+<?php
+foreach ($aVoirs as $aVoir) {
+    echo $aVoir['created_at']. '<br/>';
+}
+
+?>
+<h1>En construction</h1>
+
+<a href="index.php">Retour à l'index</a>
+
+<?php require('include/footer.php') ?>
--- js/app.js
+++ js/app.js
@@ -6,3 +6,13 @@ $('.buttonrecherche1').click(function(e) {
   $('.buttonrecherche1').addClass('hidden');
   $('.choixchoix').removeClass('hidden');
 });
+
+$('#plusDeFilm').click(function() {
+  $(location).attr('href', 'index.php')
+});
+
+
+$('#zoneTel').click(function() {
+  var win=window.open("http://www.zone-telechargement.com/", '_blank');
+  win.focus();
+});
