ed45903fcf2b2ac784a36f5207722c72c29e9f9d
Oshiiro <benjamin.delaborde@laposte.net>
2016-11-28 16:31:22 +0100

    modif ben

--- index.php
+++ index.php
@@ -12,58 +12,51 @@ $sql = "SELECT * FROM movies_full
   $randomId = $query->fetchAll();
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//                 BARRE DE RECHERCHE
+//                   BARRE DE RECHERCHE
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
 $searching = '';
 $genres = '';
 $anneesSQL = '';
 $populariteSQL = '';
 
 if(!empty($_GET['buttonrecherche2'])) {
+  $sql = '';
+  $sql .= "SELECT * FROM movies_full WHERE 1 = 1";
+
   // recherche
   if(!empty($_GET['searching'])) {
     $searching = trim(strip_tags($_GET['searching']));
+    $sql .= ' AND ( title LIKE :search OR directors LIKE :search OR cast LIKE :search )';
   }
+
   // genres
   if(!empty($_GET['genres'])) {
     $genres = $_GET['genres'];
-    function forGenres($array) {
-      foreach ($array as $key) {
-        $variable = "AND genre = '".$key."'";
-        echo $variable.'<br>';
-      };
-    }
+
+    foreach ($genres as $key) {
+      $sql .= " AND genres = '".$key."'";
+    };
+
+
   }
+
   // annees
   if(!empty($_GET['annees'])) {
     $annees = trim(strip_tags($_GET['annees']));
     $anneesStr = str_replace("-", "' AND '",$annees);
-    $anneesSQL = "'".$anneesStr."'";
+    $sql .= " AND year BETWEEN '".$anneesStr."'";
   }
+
   // popularite
   if(!empty($_GET['popularite'])) {
     $popularite = trim(strip_tags($_GET['popularite']));
     $populariteStr = str_replace("-", "' AND '",$popularite);
-    $populariteSQL = "'".$populariteStr."'";
+    $sql .= " AND rating BETWEEN '".$populariteStr."'";
   }
 
-  // debug($_GET);
-  // die();
+  $sql .= " LIMIT 20";
 
-  echo $searching.'<br>';
-  if(!empty($_GET['genres'])) {
-    forGenres($genres);
-  }
-  echo $anneesSQL.'<br>';
-  echo $populariteSQL.'<br>';
-
-  $sql = "SELECT * FROM movies_full
-          WHERE title LIKE :search
-          OR directors LIKE :search
-          OR cast LIKE :search
-          OR popularity BETWEEN $populariteSQL";
+  echo $sql;
 
   $query = $pdo->prepare($sql);
   $query->bindValue(':search','%' . $searching . '%', PDO::PARAM_STR);
@@ -74,7 +67,9 @@ if(!empty($_GET['buttonrecherche2'])) {
 
 
 }
-
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//                   FIN DE RECHERCHE
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
  ?>
