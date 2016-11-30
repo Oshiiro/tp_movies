bccd30b30c2dff6db1f6c8d7ee167740e08bc87b
Big-Bud <webforce321@gmail.com>
2016-11-30 10:46:26 +0100

    stats

--- statistiques_back.php
+++ statistiques_back.php
@@ -4,11 +4,23 @@
 
 <?php session_start() ?>
 <?php
-  $sql ='SELECT * FROM users';
+  $sql ='SELECT id FROM users WHERE 1=1';
 
   $query = $pdo->prepare($sql);
   $query->execute();
   $users = $query->fetchAll();
+
+  $sql ='SELECT id FROM movies_full WHERE 1=1';
+
+  $query = $pdo->prepare($sql);
+  $query->execute();
+  $movies = $query->fetchAll();
+
+  $sql ='SELECT pseudo FROM users ORDER BY createdat DESC';
+
+  $query = $pdo->prepare($sql);
+  $query->execute();
+  $user = $query->fetch();
 
  ?>
  <?php require('include/header_back.php') ?>
@@ -16,12 +28,12 @@
  <nav class="container-fluid">
    <div class="row">
 
-     <span class="alert alert-success col-md-3" role="alert">Nombres d'utilisateurs : <?php ?></span>
+     <span class="alert alert-success col-md-3" role="alert">Nombres d'utilisateurs : <?php echo count($users); ?></span>
 
-     <span class="alert alert-success col-md-3" role="alert">Dernier inscrit : <?php  ?></span>
+     <span class="alert alert-success col-md-3" role="alert">Dernier inscrit : <?php echo $user['pseudo']; ?></span>
    </div>
    <div class="row">
-     <span class="alert alert-success col-md-3" role="alert">Nombres de films: <?php  ?></span>
+     <span class="alert alert-success col-md-3" role="alert">Nombres de films: <?php echo count($movies); ?></span>
    </div>
  </nav>
 </div>
