8e7c5b2a15b2f0c9f38f3f6eb83c526de1ad0b25
Oshiiro <benjamin.delaborde@laposte.net>
2016-11-30 14:02:57 +0100

    modif ben

--- filmsvu.php
+++ filmsvu.php
@@ -45,26 +45,32 @@ $sql = "SELECT notes.note, movies.title, movies.plot, movies.rating, notes.statu
 
 
 // requete étoile
-if(!empty($_POST['submit'])) {
-
-$id_user = $_SESSION['user']['id'];
-$note =  $_POST['rating-input-1'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // …
+//  if(!empty($_POST['rating-input-1'])) {
 $movie = $_POST['idmovie'];
+  $id_user = $_SESSION['user']['id'];
+  $note =  $_POST['rating-input-'.$movie];
 
+  echo $note;
 
-  if(!empty($_POST['idmovie'])) {
+  echo "-" . $movie;
+  die();
 
-    echo $note;
-    echo 'movie ' .$_POST['idmovie'];
+    if(!empty($_POST['idmovie'])) {
 
-  $sql = "UPDATE movies_user_note
-          SET note = $note, status= 3
-          WHERE id_movie = $movie
-          ";
-          $query = $pdo->prepare($sql);
-          $query->execute();
+      echo $note;
+      echo 'movie ' .$_POST['idmovie'];
 
-    }
+    $sql = "UPDATE movies_user_note
+            SET note = $note, status= 3
+            WHERE id_movie = $movie
+            ";
+            $query = $pdo->prepare($sql);
+            $query->execute();
+
+      }
+  //}
 }
 
 // header('Location: filmsvu.php');
@@ -92,24 +98,23 @@ $movie = $_POST['idmovie'];
 
             <div class="rating">
             <form class="form" method="POST" action="">
-              <input type="radio" class="rating-input hidden" value="100"
-                  id="rating-input-1-5" name="rating-input-1">
+              <input type="radio" class="rating-input hidden" value="100" name="rating-input-<?php echo $filmv['id']; ?>">
               <label for="rating-input-1-5" class="rating-star"></label>
-              <input type="radio" class="rating-input hidden"  value="80"
-                  id="rating-input-1-4" name="rating-input-1">
+
+              <input type="radio" class="rating-input hidden"  value="80" name="rating-input-<?php echo $filmv['id']; ?>">
               <label for="rating-input-1-4" class="rating-star"></label>
-              <input type="radio" class="rating-input hidden"  value="60"
-                  id="rating-input-1-3" name="rating-input-1">
+
+              <input type="radio" class="rating-input hidden"  value="60" name="rating-input-<?php echo $filmv['id']; ?>">
               <label for="rating-input-1-3" class="rating-star"></label>
-              <input type="radio" class="rating-input hidden" value="40"
-                  id="rating-input-1-2" name="rating-input-1">
+
+              <input type="radio" class="rating-input hidden" value="40" name="rating-input-<?php echo $filmv['id']; ?>">
               <label for="rating-input-1-2" class="rating-star"></label>
-              <input type="radio" class="rating-input hidden" value="20"
-                  id="rating-input-1-1" name="rating-input-1">
+
+              <input type="radio" class="rating-input hidden" value="20" name="rating-input-<?php echo $filmv['id']; ?>">
               <label for="rating-input-1-1" class="rating-star"></label>
 
               <input type="hidden" name="idmovie" value="<?php echo $filmv['id']; ?>" />
-              <input type="submit" name="submit" value="Votez">
+              <input type="submit" value="Votez">
             </form>
           </div>
 
